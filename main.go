ef041afcd8a5e42d055681188dd244f6a735537e
aayam pokharel <aayam.pokharel@gmail.com>
2025-12-01 16:46:58 +0545

    feat(docker): run http server in docker

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("=============================")
 	fmt.Println("hello this is from docker PC")
 	fmt.Println("=============================")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
+	er := http.ListenAndServe(":8080", nil)
+	if er != nil {
+		fmt.Println(er)
+	}
+
 }
