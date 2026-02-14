27f7308cb192e2db46fc60221cecb588ea28ccb3
aayam pokharel <aayam.pokharel@gmail.com>
2026-02-14 14:17:58 +0545

    fix(docker):revising docker

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 func main() {
 	fmt.Println("=============================")
 	fmt.Println("hello this is from docker PC and whats up a to the b")
 	fmt.Println("=============================")
-
+	fmt.Print(uuid.New().String()[:5])
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	w.Write([]byte("hello world hello world heyyy whats up"))
 	// })
--- web.dockerfile
+++ web.dockerfile
@@ -1,6 +1,7 @@
 FROM golang:1.26.0-alpine3.22 AS builder
 WORKDIR /app
+COPY go.mod go.sum ./
+RUN ["go","mod","download"]
 COPY . . 
 RUN ["chmod","u+x","./cmd.sh"]
 CMD ["./cmd.sh"]
-
