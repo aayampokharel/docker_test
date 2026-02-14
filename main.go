ae4fc6967ce10e27076031091a1055a65c7cf885
aayam pokharel <aayam.pokharel@gmail.com>
2026-02-14 14:26:50 +0545

    fix(docker): added ports in docker compose file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/google/uuid"
 )
@@ -11,12 +12,12 @@ func main() {
 	fmt.Println("hello this is from docker PC and whats up a to the b")
 	fmt.Println("=============================")
 	fmt.Print(uuid.New().String()[:5])
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("hello world hello world heyyy whats up"))
-	// })
-	// er := http.ListenAndServe(":8080", nil)
-	// if er != nil {
-	// 	fmt.Println(er)
-	// }
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world hello world heyyy whats up"))
+	})
+	er := http.ListenAndServe(":8080", nil)
+	if er != nil {
+		fmt.Println(er)
+	}
 
 }
